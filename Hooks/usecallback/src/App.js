626c75da35638439b72f05e11bd71fcd120f5043
tugrankenger <turankenger@hotmail.com.tr>
2023-08-28 21:06:12 +0300

    edit css

--- Hooks/usecallback/src/App.js
+++ Hooks/usecallback/src/App.js
@@ -1,23 +1,27 @@
 import { useState } from 'react';
+import './App.css'
+
 import ProductPage from './components/ProductPage';
 function App() {
   const [isDark, setIsDark] = useState(false);
   return (
     <>
-      <label>
-        <input
-          type='checkbox'
-          checked={isDark}
-          onChange={(e) => setIsDark(e.target.checked)}
-        />{' '}
-        Dark mode
-      </label>
-      <hr />
-      <ProductPage
-        refferrerId='lost'
-        productId={111}
-        theme={isDark ? 'dark' : 'light'}
-      />
+      <div className='App'>
+        <label>
+          <input
+            type='checkbox'
+            checked={isDark}
+            onChange={(e) => setIsDark(e.target.checked)}
+          />{' '}
+          Dark mode
+        </label>
+        <hr />
+        <ProductPage
+          refferrerId='lost'
+          productId={111}
+          theme={isDark ? 'dark' : 'light'}
+        />
+      </div>
     </>
   );
 }
